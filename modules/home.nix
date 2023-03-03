8967149ece6c63d580f9a1c3237e72028ed8dbee
alex <alex@mcculley.tech>
2023-03-03 15:00:17 -0600

    fourth attempt, cleaned up home.nix

--- modules/home.nix
+++ modules/home.nix
@@ -9,27 +9,6 @@
     ./git
   ];
 
-  nixpkgs = {
-    # You can add overlays here
-    overlays = [
-      # If you want to use overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
-    ];
-    # Configure your nixpkgs instance
-    config = {
-      allowUnfree = true;
-      # Workaround for https://github.com/nix-community/home-manager/issues/2942
-      allowUnfreePredicate = (_: true);
-    };
-  };
-
   home = {
     username = "alex";
     homeDirectory = "/home/alex";
@@ -51,7 +30,4 @@
 
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";  
-
-  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  home.stateVersion = "22.11";
 }
