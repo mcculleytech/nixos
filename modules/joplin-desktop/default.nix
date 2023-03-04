2b0d14a39914ea5cb520febcee99a85baf6da203
alex <alex@mcculley.tech>
2023-03-03 19:34:46 -0600

    work

--- modules/joplin-desktop/default.nix
+++ modules/joplin-desktop/default.nix
@@ -1,14 +1,14 @@
 {
-  xdg.desktopEntries."@joplinapp-desktop" = {
-   # This bastard won't follow theming so I have to set it manually. 
-    name = "Joplin";
-    exec = "joplin-desktop --no-sandbox %U";
-    terminal = false;
-    type = "Application";
-    icon = "joplin";
-    startupwmclass = "Joplin";
-    mimetype="x-scheme-handler/joplin;";
-    comment = "Joplin for Desktop";
-    categories = "Office";
+  xdg.desktopEntries = {
+    "@joplinapp-desktop" = {
+     # This bastard won't follow theming so I have to set it manually. 
+      name = "Joplin";
+      exec = "joplin-desktop --no-sandbox %U";
+      terminal = false;
+      type = "Application";
+      icon = "joplin";
+      comment = "Joplin for Desktop";
+      categories = [ "Office" ];
+    };
   };
 }
